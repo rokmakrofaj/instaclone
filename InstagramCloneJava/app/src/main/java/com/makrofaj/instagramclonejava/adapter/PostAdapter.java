b6bef7611d4ceaef36238488bf5a9fab29ea9038
rokmakrofaj <84280267+rokmakrofaj@users.noreply.github.com>
2021-10-16 14:30:16 +0300

    instaclone

---
+++ InstagramCloneJava/app/src/main/java/com/makrofaj/instagramclonejava/adapter/PostAdapter.java
@@ -0,0 +1,52 @@
+package com.makrofaj.instagramclonejava.adapter;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.makrofaj.instagramclonejava.databinding.RecyclerRowBinding;
+import com.makrofaj.instagramclonejava.model.Post;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.PostHolder> {
+
+    private ArrayList<Post> postArrayList;
+
+    public PostAdapter(ArrayList<Post> postArrayList) {
+        this.postArrayList = postArrayList;
+    }
+
+    @NonNull
+    @Override
+    public PostHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        RecyclerRowBinding recyclerRowBinding= RecyclerRowBinding.inflate(LayoutInflater.from(parent.getContext()),parent,false);
+        return new PostHolder(recyclerRowBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostHolder holder, int position) {
+        holder.recyclerRowBinding.recyclerViewUserEmailText.setText(postArrayList.get(position).email);
+        holder.recyclerRowBinding.recyclerViewCommentText.setText(postArrayList.get(position).comment);
+        Picasso.get().load(postArrayList.get(position).downloadUrl).into(holder.recyclerRowBinding.recyclerViewImageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return postArrayList.size();
+    }
+
+    class PostHolder extends RecyclerView.ViewHolder{
+
+        RecyclerRowBinding recyclerRowBinding;
+        public PostHolder(RecyclerRowBinding recyclerRowBinding) {
+            super(recyclerRowBinding.getRoot());
+            this.recyclerRowBinding = recyclerRowBinding;
+        }
+    }
+
+}
